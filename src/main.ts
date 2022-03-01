8ff9cf4852bd66c6c7b8e40b42d77fd4a967e8f9
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-01 23:46:42 +0100

    feat: :heavy_plus_sign: add pinia and path dependencies
    Added Pinia storage library and path library to add a path resolution alias.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { Quasar } from 'quasar'
 
 // Import Quasar css
@@ -11,5 +12,7 @@ const app = createApp(App)
 app.use(Quasar, {
   plugins: {},
 })
+
+app.use(createPinia())
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,13 @@ export default defineConfig({
     }),
 
     quasar({
-      sassVariables: 'src/quasar-variables.sass'
+      sassVariables: 'src/quasar-variables.scss'
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      // @ts-ignore
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
