366ff20be3f94c80e1b36c3bbff30481f8070aca
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-09 20:57:58 +0100

    feat: :sparkles: added a component to show wallet address
    Added a UI component that displays your wallet address.

---
+++ src/components/WalletAddress.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="row items-center bg-rounded-translucent q-px-lg q-py-sm no-wrap">
+        <q-icon name="svguse:icons.svg#arrow-right" size="sm" class="q-mr-lg rotate-180"></q-icon>
+        <div class="q-mr-md">
+            <p class="q-ml-sm text-uppercase q-mb-none text-caption">Address</p>
+            <p class="text-weight-bold text-subtitle1 q-mb-none">bitsong1...u085</p>
+        </div>
+        <q-icon name="svguse:icons.svg#copy" size="sm"></q-icon>
+    </div>
+</template>
