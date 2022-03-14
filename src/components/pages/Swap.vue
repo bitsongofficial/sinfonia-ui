649ededb054cdce764088689b10973d6691c197b
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-15 00:31:42 +0100

    feat: :sparkles: added dark card component
    Added dark card component.

---
+++ src/components/pages/Swap.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="row">
+        <div class="col-12 col-md-7">
+            <Title class="q-mb-36">Swap Tokens</Title>
+            <Card class="q-pa-36">
+                <p class="fs-14 font-weight-medium q-mb-20">Swap from</p>
+                
+            </Card>
+        </div>
+        <div class="col-12 col-md-5">
+            
+        </div>
+    </div>
+</template>
