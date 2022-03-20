29139ce8bfaba225fa90943f22e2ce92f7ff1e98
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-20 03:02:50 +0100

    feat: :sparkles: added socials component
    Added a component to display icons for a given social object.

---
+++ src/components/Socials.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+    import { resolveIcon } from '@/common/resolvers'
+    const props = defineProps<{
+        socials?: object,
+    }>()
+</script>
+
+<template>
+    <div class="flex justify-between">
+        <div v-for="(url, social) in socials">
+            <a :href="url" class="fs-24 opacity-30 hover:opacity-100 text-white">
+                <q-icon :name="resolveIcon(social, 25, 25, 'socials')"></q-icon>
+            </a>
+        </div>
+    </div>
+</template>
