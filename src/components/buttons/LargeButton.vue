db449c607985818e73547bfbe0ce1ea0e8d77a3a
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-21 12:25:33 +0100

    fix: :bug: added a space to LargeButton classes declaration
    Added a space to fix class names merging in LargeButton.

--- src/components/buttons/LargeButton.vue
+++ src/components/buttons/LargeButton.vue
@@ -7,7 +7,7 @@
 </script>
 
 <template>
-    <StandardButton :class="'fs-16 q-py-20 text-center' + (fit ? 'w-fit q-px-40 q-py-16' : 'full-width')">
+    <StandardButton :class="'fs-16 q-py-20 text-center ' + (fit ? 'w-fit q-px-40 q-py-16' : 'full-width')">
         <slot></slot>
     </StandardButton>
 </template>
