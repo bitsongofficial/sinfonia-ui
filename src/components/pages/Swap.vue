815a5f262882d36a3cfe3a1033a8c82aa7d3a698
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-21 16:50:05 +0100

    feat: :lipstick: added custom scrollbar wip
    Work in progress custom scrollbar.

--- src/components/pages/Swap.vue
+++ src/components/pages/Swap.vue
@@ -80,7 +80,6 @@ import ExpandableCard from '../cards/ExpandableCard.vue'
     {
         if(heightRef.value && heightRef.value.element)
         {
-            console.log(heightRef.value.element.clientHeight)
             boxesStyle.value.maxHeight = ((heightRef.value.element.clientHeight - 157) / 2) + "px"
         }
     }
@@ -166,8 +165,8 @@ import ExpandableCard from '../cards/ExpandableCard.vue'
                     <p class="fs-18">DEX</p>
                     <q-btn outline rounded color="white" label="View all" class="q-px-22" />
                 </div>
-                <Card class="q-py-10 q-px-none q-mb-51" :padding="0" transparency="5">
-                    <CryptoTable virtual-scroll :style="boxesStyle" :rows="dex" :columns="columns" class="bg-transparent hide-header small-rows">
+                <Card class="q-py-10 q-px-none q-mb-51 scroll-container" :padding="0" :transparency="5" :style="boxesStyle">
+                    <CryptoTable :rows="dex" :columns="columns" class="bg-transparent hide-header small-rows">
 
                     </CryptoTable>
                 </Card>
@@ -175,8 +174,8 @@ import ExpandableCard from '../cards/ExpandableCard.vue'
                     <p class="fs-18">DEX</p>
                     <q-btn outline rounded color="white" label="View all" class="q-px-22" />
                 </div>
-                <Card class="q-py-10 q-px-none" :padding="0" transparency="5">
-                    <CryptoTable virtual-scroll :style="boxesStyle" :rows="dex" :columns="columns" class="bg-transparent hide-header small-rows">
+                <Card class="q-py-10 q-px-none overflow-auto" :padding="0" :transparency="5" :style="boxesStyle">
+                    <CryptoTable :rows="dex" :columns="columns" class="bg-transparent hide-header small-rows">
 
                     </CryptoTable>
                 </Card>
