97e084324f8fbeb57a2b379e17a395fd8186ea74
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-23 15:52:48 +0100

    feat: :sparkles: added utilities needed for modals components
    Added classes and helpers function usefull for modal components and updated old components to match them.

--- src/components/buttons/SmallButton.vue
+++ src/components/buttons/SmallButton.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
-    const props = defineProps(['label'])
+    const props = defineProps<{
+        label: string,
+        xs?: boolean
+    }>()
 </script>
 <template>
     <q-btn
         rounded
         size="sm"
         :label="props.label"
-        class="bg-gradient"
+        :class="'bg-gradient ' + (xs ? 'q-px-10 q-py-0 !fs-9' : '')"
     />
 </template>
