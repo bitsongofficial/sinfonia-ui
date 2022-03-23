c362d3da859a2262b7c75bd01eb10e16d6328a7c
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-23 22:51:53 +0100

    feat: :sparkles: added transaction table #23

--- src/components/LightTable.vue
+++ src/components/LightTable.vue
@@ -14,6 +14,7 @@
         :rows="rows"
         :columns="columns"
         :class="(noBackground ? 'table-no-background' : '')"
+        virtual-scroll
     >
         <template v-slot:header="slotProps">
             <q-tr :props="slotProps">
