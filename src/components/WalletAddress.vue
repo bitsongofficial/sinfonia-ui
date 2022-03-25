bcbe387cdf839b284bb0d1795f0663a7d159210d
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-25 02:21:11 +0100

    fix: :bug: fixed spacing in wallet component in the bottom left
    Fixed spacing in wallet component in the bottom left to adapt to new layout without back icon.

--- src/components/WalletAddress.vue
+++ src/components/WalletAddress.vue
@@ -7,15 +7,14 @@
     const authStore = useAuth()
 </script>
 <template>
-    <div v-if="authStore.session" class="row items-center bg-rounded-translucent q-px-32 q-py-16-5 no-wrap w-fit">
-        <!-- <IconButton icon="arrow-right" width="14" height="14" class="rotate-180 s-12 fs-12" @click="connected = false"></IconButton> -->
-        <div class="q-mx-15">
+    <div v-if="authStore.session" class="row items-center bg-rounded-translucent q-pl-24 q-pr-18 q-py-16-5 no-wrap w-fit">
+        <div class="q-mr-50">
             <p class="text-uppercase text-caption fs-8 opacity-40 q-mb-3">Address</p>
             <p class="text-weight-bold text-subtitle1 fs-12">bitsong1...u085</p>
         </div>
         <IconButton icon="copy" width="20" height="20" class="opacity-40 fs-15"></IconButton>
     </div>
     <div v-else>
-        <LargeButton fit @click="authStore.signIn">Connect to Keplr</LargeButton>
+        <LargeButton :padding-x="36" fit @click="authStore.signIn">Connect to Keplr</LargeButton>
     </div>
 </template>
