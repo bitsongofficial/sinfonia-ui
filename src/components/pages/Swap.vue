ddca9ca746d1f09d7079852137b03c0b4c33bf71
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2022-03-25 03:05:15 +0100

    feat: :sparkles: chanded columns breakpoint in swap page

--- src/components/pages/Swap.vue
+++ src/components/pages/Swap.vue
@@ -126,7 +126,7 @@
     {
         if(heightRef.value && heightRef.value.element)
         {
-            boxesStyle.value.height = ((heightRef.value.element.clientHeight - 157) / 2) + "px"
+            boxesStyle.value.height = ((heightRef.value.element.clientHeight - 122) / 2) + "px"
         }
     }
 
@@ -143,7 +143,7 @@
 <template>
     <div class="text-weight-medium">
         <div class="row q-col-gutter-x-xl">
-            <div class="col-8 col-md-5 q-mb-40 q-mb-md-none flex justify-center">
+            <div class="col-8 col-lg-4 col-xl-5 q-mb-40 q-mb-md-none flex justify-center">
                 <div class="max-w-582">
                     <Title class="q-mb-36">Swap Tokens</Title>
                     <Card ref="heightRef" class="q-pa-36" transparency="5">
@@ -151,67 +151,69 @@
                     </Card>
                 </div>
             </div>
-            <div class="col-8 col-md-3">
-                <div class="flex justify-between items-center q-mb-30">
-                    <p class="fs-18">DEX</p>
-                    <q-btn outline rounded color="white" label="View all" class="q-px-22" />
-                </div>
-                <Card class="q-py-10 q-px-none q-mb-51 scroll-container" :padding="0" :transparency="5" :style="boxesStyle">
-                    <CryptoTable :rows="configStore.fantokens" :columns="columns" no-background hide-header class="small-rows full-height">
-                    </CryptoTable>
-                </Card>
-                <div class="flex justify-between items-center q-mb-30">
-                    <p class="fs-18">Transactions</p>
-                    <q-btn outline rounded color="white" label="View all" class="q-px-22" />
-                </div>
-                <Card class="q-py-10 q-px-none overflow-auto items-center" :padding="0" :transparency="5" :style="boxesStyle">
-                    <LightTable
-                        :rows="transactions"
-                        :columns="transactionColumns"
-                        no-background
-                        hide-header
-                        class="full-height"
-                    >
-                        <template v-slot:body-cell-token="slotProps">
-                            <q-td :props="slotProps">
-                                <div class="flex no-wrap items-center">
-                                    <ImagePair
-                                        :image1="slotProps.row.coin1.iconUrl"
-                                        :image2="slotProps.row.coin2.iconUrl"
-                                        :size="24"
-                                        :smaller-size="20"
-                                        :offset="[-8, -1]"
+            <div class="col-8 col-lg-4 col-xl-3">
+                <div class="max-w-582 q-mx-auto">
+                    <div class="flex justify-between items-center q-mb-30">
+                        <p class="fs-18">DEX</p>
+                        <q-btn outline rounded color="white" label="View all" class="q-px-22" />
+                    </div>
+                    <Card class="q-py-10 q-px-none q-mb-51 scroll-container" :padding="0" :transparency="5" :style="boxesStyle">
+                        <CryptoTable :rows="configStore.fantokens" :columns="columns" no-background hide-header class="small-rows full-height">
+                        </CryptoTable>
+                    </Card>
+                    <div class="flex justify-between items-center q-mb-30">
+                        <p class="fs-18">Transactions</p>
+                        <q-btn outline rounded color="white" label="View all" class="q-px-22" />
+                    </div>
+                    <Card class="q-py-10 q-px-none overflow-auto items-center" :padding="0" :transparency="5" :style="boxesStyle">
+                        <LightTable
+                            :rows="transactions"
+                            :columns="transactionColumns"
+                            no-background
+                            hide-header
+                            class="full-height"
+                        >
+                            <template v-slot:body-cell-token="slotProps">
+                                <q-td :props="slotProps">
+                                    <div class="flex no-wrap items-center">
+                                        <ImagePair
+                                            :image1="slotProps.row.coin1.iconUrl"
+                                            :image2="slotProps.row.coin2.iconUrl"
+                                            :size="24"
+                                            :smaller-size="20"
+                                            :offset="[-8, -1]"
+                                        >
+                                        </ImagePair>
+                                    </div>
+                                </q-td>
+                            </template>
+                            <template v-slot:body-cell-transaction="slotProps">
+                                <q-td :props="slotProps">
+                                    <p class="fs-12 text-gray">
+                                        You swapped 
+                                        <span class="text-white font-weight-500 q-mx-4">{{balancedCurrency(slotProps.row.amount)}} {{slotProps.row.coin1.symbol}}</span>
+                                        in 
+                                        <span class="text-white font-weight-500 q-mx-4">{{balancedCurrency(slotProps.row.amount * slotProps.row.rate)}} {{slotProps.row.coin2.symbol}}</span>
+                                    </p>
+                                </q-td>
+                            </template>
+                            <template v-slot:body-cell-status="slotProps">
+                                <q-td :props="slotProps">
+                                <div class="flex justify-end">
+                                    <p
+                                        v-if="slotProps.row.status != 'completed'"
+                                        :class="'rounded-20 q-py-9 q-px-8 text-white text-capitalize fs-9 ' +
+                                        (slotProps.row.status == 'pending' ? 'bg-dark' : 'bg-primary')"
                                     >
-                                    </ImagePair>
+                                        {{slotProps.row.status}}
+                                    </p>
+                                    <p v-else class="fs-9 text-gray">{{formatDistanceToNow(new Date(slotProps.row.time*1000))}}</p>
                                 </div>
-                            </q-td>
-                        </template>
-                        <template v-slot:body-cell-transaction="slotProps">
-                            <q-td :props="slotProps">
-                                <p class="fs-12 text-gray">
-                                    You swapped 
-                                    <span class="text-white font-weight-500 q-mx-4">{{balancedCurrency(slotProps.row.amount)}} {{slotProps.row.coin1.symbol}}</span>
-                                     in 
-                                    <span class="text-white font-weight-500 q-mx-4">{{balancedCurrency(slotProps.row.amount * slotProps.row.rate)}} {{slotProps.row.coin2.symbol}}</span>
-                                </p>
-                            </q-td>
-                        </template>
-                        <template v-slot:body-cell-status="slotProps">
-                            <q-td :props="slotProps">
-                            <div class="flex justify-end">
-                                <p
-                                    v-if="slotProps.row.status != 'completed'"
-                                    :class="'rounded-20 q-py-9 q-px-8 text-white text-capitalize fs-9 ' +
-                                    (slotProps.row.status == 'pending' ? 'bg-dark' : 'bg-primary')"
-                                >
-                                    {{slotProps.row.status}}
-                                </p>
-                                <p v-else class="fs-9 text-gray">{{formatDistanceToNow(new Date(slotProps.row.time*1000))}}</p>
-                            </div>
-                            </q-td>
-                        </template>
-                    </LightTable>
-                </Card>
+                                </q-td>
+                            </template>
+                        </LightTable>
+                    </Card>
+                </div>
             </div>
         </div>
     </div>
