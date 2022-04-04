43296f005b3b7c0e4bd47e68e63f4e4a99ccff1f
EnricoBarbieri1997 <enrico.barbieri.1997@gmail.com>
2022-04-04 17:16:20 +0200

    feat: :sparkles: added link params to notification functions
    Added a link params with a text and an url to show below the second message.

--- src/common/notifications.ts
+++ src/common/notifications.ts
@@ -3,10 +3,14 @@ import { resolveIcon } from "./resolvers"
 import Spinner from "@/components/Spinner"
 import { NotificationPosition } from "@/types"
 
-function notify(
+export function notify(
 	message: string,
-	secondaryMessage: string,
-	success,
+	secondaryMessage: string | undefined,
+	link: {
+		text: string,
+		url: string,
+	} | undefined,
+	success: boolean | undefined,
 	position: NotificationPosition = "top",
 	timeout = 10000
 ) {
@@ -19,12 +23,14 @@ function notify(
 		classes: `${background} rounded-20 q-pt-20 q-pb-18 q-px-30 min-w-440`,
 		message:
 			'<div class="q-mr-15"><p class="fs-16 text-weight-medium' +
-			(secondaryMessage ? " q-mb-10" : "") +
+			(secondaryMessage != undefined || link != undefined ? " q-mb-10" : "") +
 			'">' +
-			message +
-			'</p><p class="fs-14 opacity-40">' +
-			secondaryMessage +
-			"</p></div>",
+			message + '</p>' +
+			(secondaryMessage ? ('<p class="fs-14 opacity-40' + (link ? " q-mb-10" : "") + '">' +
+				secondaryMessage +
+				'</p>') : '') +
+			(link ? '<a class="opacity-40 flex items-center hover:opacity-70 fs-14" href="' + link.url + '"><span class="q-mr-6">' + link.text +
+				'</span><svg class="s-8" viewBox="0 0 10 10"><use xlink:href="icons.svg#external"></use></svg></a>' : '') + '</div>',
 		spinner: success === undefined ? Spinner : false,
 		icon: success ? resolveIcon("success", 32, 32) : resolveIcon("error", 30, 30),
 		position,
@@ -36,24 +42,27 @@ function notify(
 export function notifySuccess(
 	message: string,
 	secondaryMessage = "",
+	link: { text: string, url: string } | undefined,
 	position: NotificationPosition = "top"
 ) {
-	return notify(message, secondaryMessage, true, position)
+	return notify(message, secondaryMessage, link, true, position)
 }
 
 export function notifyError(
 	message: string,
 	secondaryMessage = "",
-	position: NotificationPosition = "top"
+	link: { text: string, url: string } | undefined,
+	position: NotificationPosition = "top",
 ) {
-	return notify(message, secondaryMessage, false, position)
+	return notify(message, secondaryMessage, link, false, position)
 }
 
 export function notifyLoading(
 	message: string,
 	secondaryMessage = "",
+	link: { text: string, url: string } | undefined,
 	position: NotificationPosition = "top",
-	timeout = 0
+	timeout = 0,
 ) {
-	return notify(message, secondaryMessage, undefined, position, timeout)
+	return notify(message, secondaryMessage, link, undefined, position, timeout)
 }
