b85d1e34836996813a5c178af37f3d12d6ab1027
DavideSegullo <davidesegullo@gmail.com>
2022-04-19 21:49:41 +0200

    feat: :speech_balloon: updated liquidity mining text #139

--- src/components/pages/Pool.vue
+++ src/components/pages/Pool.vue
@@ -306,8 +306,8 @@ onUnmounted(() => {
 				<h3 class="fs-21 q-mb-20 q-mt-0">Liquidity Mining</h3>
 				<p class="fs-16 opacity-40 !leading-20">
 					Liquidity mining is a decentralized finance mechanism wherein participants
-					provide some of their assets into various liquidity pools, from which you
-					are rewarded with tokens and fees.
+					provide some of their crypto assets into various liquidity pools, from
+					which they're rewarded with tokens and fees.
 				</p>
 			</div>
 			<div class="col-5 column items-end">
