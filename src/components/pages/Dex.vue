552cc4092fe27529dceaaa06076fff3108bab021
DavideSegullo <davidesegullo@gmail.com>
2022-07-23 20:59:13 +0200

    feat: :sparkles: add feature #390

--- src/components/pages/Dex.vue
+++ src/components/pages/Dex.vue
@@ -1,13 +1,29 @@
 <script setup lang="ts">
-import { balancedCurrency as balancedCurrency } from "@/common/numbers"
-import useConfig from "@/store/config"
+import { balancedCurrency, resolveIcon } from "@/common"
 import { TokenBalance } from "@/types"
 import { TableColumn } from "@/types/table"
 import { useRouter } from "vue-router"
-import CryptoTable from "../CryptoTable.vue"
+import { computed, ref } from "vue"
+import { reduce } from "lodash"
+import { BigNumber } from "bignumber.js"
+import useConfig from "@/store/config"
+import CryptoTable from "@/components/CryptoTable.vue"
+import Title from "@/components/typography/Title.vue"
+import Tabs from "@/components/Tabs.vue"
 
 const configStore = useConfig()
 const router = useRouter()
+const fantokensType = ref("all")
+const searchFocussed = ref(false)
+const searchValue = ref("")
+
+const searchActive = computed(() => {
+	return searchValue.value.length > 0 || searchFocussed.value
+})
+
+const focussed = () => {
+	searchFocussed.value = true
+}
 
 const columns: TableColumn[] = [
 	{
@@ -16,8 +32,8 @@ const columns: TableColumn[] = [
 		label: "",
 		align: "left",
 		field: "rank",
-		headerClasses: "w-5",
-		classes: "w-5",
+		headerClasses: "w-1 hidden",
+		classes: "w-1",
 	},
 	{
 		name: "token",
@@ -25,6 +41,7 @@ const columns: TableColumn[] = [
 		label: "Token",
 		field: "name",
 		sortable: true,
+		colspan: "2",
 	},
 	{
 		name: "symbol",
@@ -69,12 +86,90 @@ const onRowClick = (_, row: TokenBalance, index: number) => {
 		router.push(`/fantokens/${coinLookup.fantokenDenom}`)
 	}
 }
+
+const totalMarketCap = computed(() => {
+	const ttlMC = reduce(
+		configStore.fantokensRanking,
+		(prev, curr) => {
+			const currMarketCap = new BigNumber(curr.marketCap ?? 0)
+
+			return prev.plus(currMarketCap)
+		},
+		new BigNumber(0)
+	)
+
+	return balancedCurrency(ttlMC.toString())
+})
+
+const tabs = [
+	{ name: "all", label: "All Tokens" },
+	{ name: "artist", label: "Artists" },
+	{ name: "label", label: "Labels" },
+	{ name: "magazine", label: "Magazines" },
+]
+
+const fantokens = computed(() =>
+	configStore.fantokensRanking
+		.filter((fantoken) => {
+			if (fantokensType.value === "all") {
+				return true
+			}
+
+			const tags = fantoken.metadata?.tags ?? []
+
+			return tags.includes(fantokensType.value)
+		})
+		.filter((fantoken) => {
+			if (!searchActive.value) {
+				return true
+			}
+
+			return fantoken.name
+				.toLocaleLowerCase()
+				.includes(searchValue.value.toLocaleLowerCase())
+		})
+)
 </script>
 <template>
-	<h3 class="q-mb-xl fs-27 light:text-weight-medium q-mt-0">FanTokens</h3>
+	<Title class="q-mb-8">FanTokens</Title>
+	<div class="row items-center q-mb-24">
+		<p class="fs-16 !leading-24 q-mr-14 opacity-40">The total market cap is</p>
+
+		<p class="fs-21 !leading-24 text-weight-medium text-gradient">
+			$ {{ totalMarketCap }}
+		</p>
+	</div>
+	<div class="row items-center justify-between q-mb-42">
+		<div class="q-mt-8">
+			<Tabs v-model="fantokensType" :options="tabs" border />
+		</div>
+
+		<div
+			@click="focussed"
+			@focusout="searchFocussed = false"
+			:class="'relative-position cursor-pointer group'"
+		>
+			<div
+				class="absolute-full bg-white rounded-30 opacity-5 light:opacity-100 shadow-md"
+			></div>
+			<div class="flex items-center q-px-28 q-py-14">
+				<q-input
+					class="q-mr-4 min-size-input"
+					input-class="q-py-0"
+					hide-bottom-space
+					borderless
+					v-show="searchActive"
+					v-model="searchValue"
+					:debounce="500"
+					dense
+				/>
+				<q-icon size="15px" :name="resolveIcon('search', 13, 13)"></q-icon>
+			</div>
+		</div>
+	</div>
 	<CryptoTable
 		:columns="columns"
-		:rows="configStore.fantokensRanking"
+		:rows="fantokens"
 		:pagination="pagination"
 		@row-click="onRowClick"
 	/>
