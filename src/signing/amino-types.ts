ee1cb9eebeca1fce55dc9de25ea5caeede0f7e66
DavideSegullo <davidesegullo@gmail.com>
2022-10-01 17:26:22 +0200

    feat: :sparkles: add custom type amino for unlock and lock messages
    the types provided in the osmojs library are wrong, for this reason I am using custom types

--- src/signing/amino-types.ts
+++ src/signing/amino-types.ts
@@ -1,6 +1,8 @@
 import { AminoConverters } from "@cosmjs/stargate"
 import { AminoMsg } from "@cosmjs/amino"
 import { MsgClaim } from "./codec/bitsong/merkledrop/v1beta1/tx"
+import { Coin } from "@cosmjs/launchpad"
+import { osmosis } from "osmojs"
 import Long from "long"
 
 export interface AminoMsgClaim extends AminoMsg {
@@ -12,6 +14,41 @@ export interface AminoMsgClaim extends AminoMsg {
 		amount: string
 		proofs: string[]
 	}
+}
+
+export interface AminoMsgLockTokens {
+	readonly type: "osmosis/lockup/lock-tokens"
+	readonly value: {
+		owner: string // Owner
+		duration: string // Duration's unit is expected to be the second.
+		coins: Coin[]
+	}
+}
+
+export interface MsgLockTokens {
+	owner: string // Owner
+	duration: {
+		seconds: Long
+		nanos: number
+	} // Duration's unit is expected to be the second.
+	coins: Coin[]
+}
+
+export interface AminoMsgBeginUnlocking extends AminoMsg {
+	type: "osmosis/lockup/begin-unlock-period-lock"
+	value: {
+		owner: string
+		ID: string
+		coins: {
+			denom: string
+			amount: string
+		}[]
+	}
+}
+
+export interface MsgBeginUnlocking {
+	owner: string
+	ID: Long
 }
 
 export const createBitsongAminoConverters = (): AminoConverters => {
@@ -48,6 +85,43 @@ export const createBitsongAminoConverters = (): AminoConverters => {
 					proofs,
 				}
 			},
+		},
+		"/osmosis.lockup.MsgLockTokens": {
+			aminoType: "osmosis/lockup/lock-tokens",
+			toAmino: ({ owner, duration, coins }: MsgLockTokens) => {
+				return {
+					owner,
+					duration: (duration.seconds.toInt() * 1_000_000_000).toString(),
+					coins,
+				}
+			},
+			fromAmino: ({ owner, duration, coins }: AminoMsgLockTokens["value"]) => {
+				const msgDuration = parseInt(duration)
+
+				return {
+					owner,
+					duration: {
+						seconds: Long.fromNumber(Math.floor(msgDuration / 1_000_000_000)),
+						nanos: msgDuration % 1_000_000_000,
+					},
+					coins,
+				}
+			},
+		},
+		"/osmosis.lockup.MsgBeginUnlocking": {
+			aminoType: "osmosis/lockup/begin-unlock-period-lock",
+			toAmino: ({ owner, ID }: MsgBeginUnlocking) => {
+				return {
+					owner,
+					ID: ID.toString(),
+					coins: [],
+				}
+			},
+			fromAmino: ({ owner, ID }: AminoMsgBeginUnlocking["value"]) => ({
+				owner,
+				ID,
+				coins: [],
+			}),
 		},
 	}
 }
