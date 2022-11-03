9a2b441fcd849917c4c29ceedb79ffd4f360dd3d
DavideSegullo <davidesegullo@gmail.com>
2022-11-03 18:15:41 +0100

    feat: :sparkles: add new layout for collection details page

--- src/components/pages/collections/Collection.vue
+++ src/components/pages/collections/Collection.vue
@@ -2,15 +2,17 @@
 import useConfig from "@/store/config"
 import useNFT from "@/store/nft"
 import useSettings from "@/store/settings"
-import Tabs from "@/components/Tabs.vue"
-import CollectionSocials from "@/components/CollectionSocials.vue"
+import Spinner from "@/components/Spinner"
+import Card from "@/components/cards/Card.vue"
+import IconButton from "@/components/buttons/IconButton.vue"
 import Title from "@/components/typography/Title.vue"
 import NFTCard from "@/components/cards/NFTCard.vue"
 import LargeButton from "@/components/buttons/LargeButton.vue"
-import { isValidContractAddress } from "@/common"
+import { formatShortAddress, isValidContractAddress } from "@/common"
 import { computed, onMounted, onUnmounted, watch } from "vue"
 import { useRoute, useRouter } from "vue-router"
 import { FantokenTab } from "@/types"
+import useClipboard from "@/hooks/useClipboard"
 
 const route = useRoute()
 const router = useRouter()
@@ -51,117 +53,108 @@ onUnmounted(() => {
 	collectionWatcher()
 })
 
-const topImageStyle = computed(
-	() =>
-		`background: linear-gradient(360deg, #220D32 3.59%, rgba(34, 13, 50, 0) 176.73%), url(${
-			collection.value?.metadata?.cover ?? ""
-		});`
-)
-
-const topImageStyleLight = computed(
-	() =>
-		`background: linear-gradient(360deg, #F0EDF2 3.59%, rgba(240, 237, 242, 0) 100.73%), url(${
-			collection.value?.metadata?.cover ?? ""
-		});`
-)
-
-const tabs = computed(() => {
-	const links: FantokenTab[] = [{ name: "nfts", label: "NFTs" }]
-
-	links.push({ name: "details", label: "Details" })
-
-	links.push({ name: "social", label: "Social" })
-
-	return links
-})
+const { onCopy } = useClipboard()
 </script>
 
 <template>
 	<div class="text-white text-weight-medium" v-if="collection">
-		<div
-			class="absolute-top full-width -z-1 hv-3/5 !bg-cover !bg-center"
-			:style="$q.dark.isActive ? topImageStyle : topImageStyleLight"
-		>
-			<div
-				class="absolute left-0 top-99 full-width window-height main-page-background-helper"
-			></div>
+		<div class="w-full position-relative q-mb-60">
+			<q-img
+				class="w-full rounded-20"
+				:src="collection.metadata?.cover"
+				ratio="16/9"
+				alt="Cover"
+				title="Cover"
+				width="100%"
+				height="300px"
+			/>
+
+			<img
+				class="absolute-bottom !left-24 !-bottom-24 border-white-2 w-126 h-126 rounded-full object-cover z-1"
+				:src="collection.metadata?.image"
+			/>
 		</div>
-		<div class="row q-mb-70">
-			<div class="col-8 col-md-4">
-				<div class="flex q-mb-60 items-start items-center-xs column-xs">
-					<q-avatar size="120px" class="q-mr-40 q-mr-xs-0 q-mb-14">
-						<img :src="collection?.metadata?.image" :alt="collection?.init?.name" />
-					</q-avatar>
-					<div class="text-center-xs">
-						<p class="text-dark q-mb-18 fs-21">
-							{{ collection?.init?.name }}
+		<div class="q-mb-70 grid grid-cols-12 gap-24">
+			<div class="col-span-12 col-span-md-7">
+				<Title :font-size="32" class="q-mb-16">{{ collection?.init?.name }}</Title>
+
+				<p class="text-dark q-mb-16">
+					<span class="opacity-40">Created by</span>
+					{{ formatShortAddress(collection.creator) }}
+				</p>
+
+				<Title
+					v-if="collection.metadata && collection.metadata.description"
+					:font-size="18"
+					class="q-mb-32"
+					>{{ collection.metadata.description }}</Title
+				>
+
+				<LargeButton
+					:padding-x="30"
+					:padding-y="14"
+					fit
+					:to="`/collections/${address}/mint`"
+				>
+					Mint NFT
+				</LargeButton>
+			</div>
+			<div class="col-span-12 col-span-md-3 col-start-md-10">
+				<Card
+					:padding="30"
+					:transparency="5"
+					shadow="none"
+					class="full-width text-white transition-all light:bg-white full-height !flex column justify-between no-wrap"
+				>
+					<div class="column">
+						<p class="fs-12 text-weight-medium opacity-40 q-pb-10">Items</p>
+						<p class="fs-16 !leading-20 text-weight-medium work-break-all">
+							{{ NFTStore.nfts.length }}
 						</p>
-						<p class="fs-60 q-mb-20 text-weight-bold">
-							{{ collection?.init?.symbol }}
+					</div>
+					<q-separator class="q-my-16 opacity-50" />
+					<div class="column">
+						<p class="fs-12 !leading-14 text-weight-medium opacity-40 q-pb-10">
+							Address
 						</p>
-						<div class="flex column-xs row items-center-xs items-end justify-center">
-							<div class="flex column q-mb-xs-14">
-								<p class="text-dark fs-16 text-uppercase q-mb-16">NFTs</p>
-								<p class="fs-32">{{ NFTStore.nfts.length }}</p>
-							</div>
-							<LargeButton
-								:padding-x="30"
-								:padding-y="14"
-								class="q-ml-xs-0 q-ml-46"
-								fit
-								:to="`/collections/${address}/mint`"
-							>
-								Mint NFT
-							</LargeButton>
+						<div class="row justify-between">
+							<p class="fs-16 !leading-20 text-weight-medium work-break-all">
+								{{ formatShortAddress(collection.address) }}
+							</p>
+
+							<IconButton
+								icon="copy"
+								width="20"
+								height="20"
+								class="text-primary-light fs-16"
+								v-if="collection"
+								@click="onCopy(collection?.address)"
+							/>
 						</div>
 					</div>
-				</div>
+				</Card>
 			</div>
 		</div>
-		<Tabs :options="tabs">
-			<template v-slot:nfts>
-				<div>
-					<Spinner v-if="NFTStore.loadingNFTs" class="!w-50 !h-50 q-mx-auto" />
-					<div
-						v-else
-						class="grid grid-cols-min-xs-1 grid-cols-2 grid-cols-md-3 grid-cols-lg-4 grid-gap-30 q-mb-74"
-					>
-						<RouterLink
-							v-for="(nft, index) in NFTStore.bitsongNFTs"
-							:key="index"
-							:to="`/collections/${address}/details/nft/${nft.token_id}`"
-							class="block full-height"
-						>
-							<NFTCard :nft="nft" />
-						</RouterLink>
-					</div>
-				</div>
-			</template>
-			<template
-				v-slot:social
-				v-if="collection.metadata && collection.metadata.external_urls"
+
+		<div class="column row-md align-items-end-md q-mb-42">
+			<Title class="q-mr-32">NFTs</Title>
+		</div>
+
+		<Spinner v-if="NFTStore.loadingNFTs" class="!w-50 !h-50 q-mx-auto" />
+
+		<div
+			v-else
+			class="grid grid-cols-min-xs-1 grid-cols-2 grid-cols-md-3 grid-cols-lg-4 grid-gap-30 q-mb-74"
+		>
+			<RouterLink
+				v-for="(nft, index) in NFTStore.bitsongNFTs"
+				:key="index"
+				:to="`/collections/${address}/details/nft/${nft.token_id}`"
+				class="block full-height"
 			>
-				<div class="row q-pt-18 q-col-gutter-x-md">
-					<div class="col-8 col-md-2 column justify-between">
-						<p class="fs-18 opacity-50 gt-sm">Follow on socials</p>
-					</div>
-					<div
-						class="col-8 col-md-4"
-						v-if="collection.metadata && collection.metadata.external_urls"
-					>
-						<p class="fs-18 opacity-50 lt-md q-mb-8">Follow on socials</p>
-						<CollectionSocials :external-links="collection.metadata.external_urls" />
-					</div>
-				</div>
-			</template>
-			<template v-slot:details>
-				<div v-if="collection.metadata">
-					<Title :font-size="24">Description</Title>
-					<Title class="text-dark q-mt-16" :font-size="16">{{
-						collection.metadata.description
-					}}</Title>
-				</div>
-			</template>
-		</Tabs>
+				<NFTCard :nft="nft" />
+			</RouterLink>
+		</div>
 	</div>
+	<Spinner v-else-if="NFTStore.loading" class="!w-50 !h-50 q-mx-auto" />
 </template>
