07465c79e03b8134ee152399ae05782718a67fc3
DavideSegullo <davidesegullo@gmail.com>
2022-11-03 17:33:04 +0100

    feat: :sparkles: add collections tabs

--- src/components/pages/collections/Collections.vue
+++ src/components/pages/collections/Collections.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import Title from "@/components/typography/Title.vue"
 import CollectionCard from "@/components/cards/CollectionCard.vue"
-import SmallButton from "@/components/buttons/SmallButton.vue"
+import LargeButton from "@/components/buttons/LargeButton.vue"
 import useNFT from "@/store/nft"
 import Spinner from "@/components/Spinner"
-import { onMounted } from "vue"
+import Tabs from "@/components/Tabs.vue"
+import { onMounted, ref } from "vue"
 import { useRoute, RouterLink } from "vue-router"
 
 const route = useRoute()
 const NFTStore = useNFT()
+const collectionsType = ref("all")
 
 const code = route.params.codeId
 	? parseInt(route.params.codeId as string, 10)
@@ -18,48 +20,49 @@ onMounted(() => {
 	NFTStore.loadCollections(code)
 })
 
+const tabs = [
+	{ name: "all", label: "All Collections" },
+	{ name: "mycollections", label: "My Collections" },
+]
+
 // TODO: Add virtual scroll
 </script>
 <template>
 	<div>
-		<template v-if="NFTStore.myCollections.length > 0">
-			<div class="column row-md align-items-end-md q-mb-42">
-				<Title class="q-mr-32">
-					My Collections
-
-					<SmallButton
-						class="q-ml-20"
-						label="Create"
-						:to="`/collections/${code}/create`"
-					/>
-				</Title>
-			</div>
-			<div
-				v-if="!NFTStore.loading"
-				class="grid grid-cols-min-xs-1 grid-cols-2 grid-cols-md-3 grid-cols-lg-4 grid-gap-30 q-mb-74"
-			>
-				<RouterLink
-					v-for="(collection, index) in NFTStore.myCollections"
-					:key="index"
-					:to="`/collections/${collection.address}/details`"
-					class="block full-height"
-				>
-					<CollectionCard :collection="collection" />
-				</RouterLink>
-			</div>
-		</template>
 		<div class="column row-md align-items-end-md q-mb-42">
-			<Title class="q-mr-32">
-				Collections
-
-				<SmallButton
-					class="q-ml-20"
-					label="Create"
-					:to="`/collections/${code}/create`"
-				/>
-			</Title>
+			<Title class="q-mr-32">Collections</Title>
 		</div>
+		<div class="row items-center justify-between q-mb-42">
+			<div class="q-mt-8">
+				<Tabs v-model="collectionsType" :options="tabs" border />
+			</div>
+
+			<LargeButton
+				label="Create Collection"
+				:to="`/collections/${code}/create`"
+				fit
+			/>
+		</div>
+
 		<Spinner v-if="NFTStore.loading" class="!w-50 !h-50 q-mx-auto" />
+
+		<template v-if="collectionsType === 'mycollections'">
+			<template v-if="NFTStore.myCollections.length > 0">
+				<div
+					v-if="!NFTStore.loading"
+					class="grid grid-cols-min-xs-1 grid-cols-2 grid-cols-md-3 grid-cols-lg-4 grid-gap-30 q-mb-42"
+				>
+					<RouterLink
+						v-for="(collection, index) in NFTStore.myCollections"
+						:key="index"
+						:to="`/collections/${collection.address}/details`"
+						class="block full-height"
+					>
+						<CollectionCard :collection="collection" />
+					</RouterLink>
+				</div>
+			</template>
+		</template>
 		<div
 			v-else
 			class="grid grid-cols-min-xs-1 grid-cols-2 grid-cols-md-3 grid-cols-lg-4 grid-gap-30 q-mb-74"
