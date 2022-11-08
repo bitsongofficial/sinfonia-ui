56b40d285dcf6639d54832caa80e9a54e1f84209
angelorc <angelo@rcstart.it>
2022-11-08 14:22:09 +0100

    test build cloudflare pages script

---
+++ build-cf-pages.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+
+if [ "$CF_PAGES_BRANCH" == "production" ]; then
+  # Run the "production" script in `package.json` on the "production" branch
+  # "production" should be replaced with the name of your Production branch
+
+  npm install -g pnpm && pnpm i && pnpm run build:mainnet
+
+elif [ "$CF_PAGES_BRANCH" == "staging" ]; then
+  # Run the "staging" script in `package.json` on the "staging" branch
+  # "staging" should be replaced with the name of your specific branch
+
+  npm install -g pnpm && pnpm i && pnpm run build
+
+fi
