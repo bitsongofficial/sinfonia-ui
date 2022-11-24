afac423b9071d20eeeedfd10d83868a5fae736f7
angelorc <angelo@rcstart.it>
2022-11-24 18:13:26 +0100

    add nft featured

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -14,6 +14,10 @@ export const coinsConfig = {
 }
 
 export const collectionsWhitelist = [
+	"bitsong1pnzsahkfp9h3gtwkuhsps6xjm5wz5885hpl27atvaqdn7e23vrdq9g0psl",
+	"bitsong1xwu4c2apw2nxd75leph52aaw85ryhpfqcwfqtyjtz38tvjln7x7qje7xzd",
+	"bitsong1387uc3z2zs3663d06f59vsn5ams2x6e33gqpghjkmhafjqsc9a3ql73qym",
+	"bitsong16p5fm4qrpjpxd8f5wpt48fgyyd5c79q4n35fgvq7x2yks2lu8llq3kre6f",
 	"bitsong18a0pvw326fydfdat5tzyf4t8lhz0v6fyfaujpeg07fwqkygcxejsv0vqlr",
 	"bitsong19jq6mj84cnt9p7sagjxqf8hxtczwc8wlpuwe4sh62w45aheseueststug5",
 ]
